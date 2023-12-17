b8ecea950d8c767e0afd805f5ffa0935c1cab1b7
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-17 23:44:20 +0530

    vehiceRepo create

---
+++ Travel-Service-Api/src/main/java/com/travelserviceapi/travelserviceapi/repo/Driver.java
@@ -0,0 +1,6 @@
+package com.travelserviceapi.travelserviceapi.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Driver extends JpaRepository<Driver,String> {
+}
