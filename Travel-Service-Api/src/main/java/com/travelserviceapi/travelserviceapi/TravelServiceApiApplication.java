3639e29726cfe11923a78361aea3fa6a7c0ed9fb
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-27 10:20:26 +0530

    create userRoleRepo interface

--- Travel-Service-Api/src/main/java/com/travelserviceapi/travelserviceapi/TravelServiceApiApplication.java
+++ Travel-Service-Api/src/main/java/com/travelserviceapi/travelserviceapi/TravelServiceApiApplication.java
@@ -2,12 +2,13 @@ package com.travelserviceapi.travelserviceapi;
 
 import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TravelServiceApiApplication {
+public class TravelServiceApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelServiceApiApplication.class, args);
@@ -24,4 +25,10 @@ public class TravelServiceApiApplication {
         return new Gson();
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+    //save user role
+    //save default user (must be admin)
+    }
 }
---
+++ Travel-Service-Api/src/main/java/com/travelserviceapi/travelserviceapi/repo/UserRoleRepo.java
@@ -0,0 +1,12 @@
+package com.travelserviceapi.travelserviceapi.repo;
+
+
+import com.travelserviceapi.travelserviceapi.entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRoleRepo extends JpaRepository<UserRole,String> {
+}
